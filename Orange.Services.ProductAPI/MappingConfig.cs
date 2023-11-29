0305ad7e721f64c0ac44cf476583f077ab3207f4
Shakeeb <shakeebparavannur@gmail.com>
2023-11-29 12:35:56 +0530

    add:ProductRepository

---
+++ Orange.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Orange.Services.ProductAPI.Models;
+using Orange.Services.ProductAPI.Models.Dto;
+
+namespace Orange.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+            return mappingConfig
+        }
+    }
+}
